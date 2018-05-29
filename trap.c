c3734d0e0c042c34a9eddbc41e74c6081eb7f074
Shreya Kumar <skuma021@ucr.edu>
2018-05-28 20:49:42 -0700

    Update trap.c

--- trap.c
+++ trap.c
@@ -50,7 +50,7 @@ trap(struct trapframe *tf)
   //T_PGFLT is part of CS153, lab3
   case T_PGFLT:
     if(rcr2() >= (KERNBASE - 1) - ((PGSIZE * myproc()->stackSize + 1) + 1)){
-      if(allocuvm(myproc()->pgdir, PGROUNDDOWN(rcr2()), PGROUNDDOWN(rcr2()) + 8) == 0){
+      if(allocuvm(myproc()->pgdir, KERNBASE - (myproc()->stack_sz + 1)*PGSIZE, KERNBASE - (myproc()->stack_sz)*PGSIZE - 1) == 0){
 	cprintf("this is bad \n");
 	freevm(myproc()->pgdir);
 	break;
