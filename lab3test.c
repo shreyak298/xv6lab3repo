7f1e7a88df2018b72c070b8861b077be537dffed
Shreya Kumar <skuma021@ucr.edu>
2018-05-28 17:40:46 -0700

    Update lab3test.c

--- lab3test.c
+++ lab3test.c
@@ -5,13 +5,16 @@
 int test(int n)
 { 
    int x = n + 1;
+   cprintf("entered test");
    return x;
 }
 int main(int argc, char *argv[])
 {
    int pid=0;
    pid=fork();
+   
    if(pid==0){
+       cprintf("pid = 0");
        test(1);
        exit();
    }
